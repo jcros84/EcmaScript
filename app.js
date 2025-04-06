9ef9f8ae712e9948d913b0b67bcd8d10c3f05861
JuanCRM <jcros84@gmail.com>
2025-04-06 21:48:16 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+
+//Ejercicio video 8
+/*var funciones = [];
+
+for (let i = 0; i < 10; i++) {
+  funciones.push(function () {
+    console.log(i);
+  });
+}
+
+funciones.forEach(function (func){
+    func();
+}  );
+*/
+
+const OPciones={};
+OPciones.audio = true;
+OPciones.video = true;
+console.log(OPciones);
