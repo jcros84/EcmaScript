d714416890a6829cc78952e5ea69d332ff8828c1
JuanCRM <jcros84@gmail.com>
2025-04-22 19:39:02 +0200

    video 14

--- app.js
+++ app.js
@@ -19,4 +19,37 @@ console.log(OPciones);
 
 var nombre = "Juan";
 console.log(nombre.startsWith("J"));
+
+
+var nombre = "Juan Castillo";
+console.log(nombre.endsWith("llo"));
+console.log(nombre.includes("llo"));
+console.log(nombre.startsWith("C",5));
+
+const espacios = 12;
+let nombre = ["Juan", "Pedro Ant", "MariaAN"];
+let edad = [25, 30, 35];
+
+for (i in nombre) {
+  console.log(nombre[i] + " ".repeat(espacios - nombre[i].length) + "| " + edad[i]);;
+}
+
+
+var nombre = "Juan";
+var apellido = "fernandez";
+
+var nombreCompleto = ` El nombre es ${nombre} ${apellido}`;
+console.log(nombreCompleto);
+nombre="Pedro";
+apellido="Sanchez";
+console.log(nombreCompleto);
+
+function saludar (fn = saludo){
+    console.log("Hola " + fn());
+}
+
+function saludo(nombre = "Carlos"){
+    return nombre;
+}
+saludar();
 */
